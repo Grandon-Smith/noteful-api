4e57151b4bb8a33330e26c6d7974987e9878242e
Grandon Smith <grandon.smith@yahoo.com>
2020-12-30 17:28:30 -0500

    so much set up to get a back end working

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (folderId, folderName)
+VALUES
+    ('1', 'folder1'),
+    ('2', 'folder2'),
+    ('3', 'folder3');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,17 @@
+INSERT INTO noteful_notes (id, name, content, modified, folderId)
+VALUES
+    ('cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1',
+      'Dogs',
+      '2019-01-03T00:00:00.000Z',
+      '1',
+      'content'),
+      ('dbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1',
+      'cat',
+      '2019-01-03T00:00:00.000Z',
+      '2',
+      'contentsss'),
+      ('cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1',
+      'Drat',
+      '2019-01-03T00:00:00.000Z',
+      '3',
+      'contentss'),
